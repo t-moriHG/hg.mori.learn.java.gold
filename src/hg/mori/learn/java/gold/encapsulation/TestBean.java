4abc1e552d5bd0237105ee68506e5e89118e85de
t-mori2015 <t-mori@hg.locopo-jp.com>
2018-12-29 14:54:39 +0900

    文字化け解消

--- src/hg/mori/learn/java/gold/encapsulation/TestBean.java
+++ src/hg/mori/learn/java/gold/encapsulation/TestBean.java
@@ -1,29 +1,29 @@
 package hg.mori.learn.java.gold.encapsulation;
 /*
- * �J�v�Z�����ɂ���
- * �t�B�[���h�̕ϐ����𒼐ڕҏW�����Ȃ���
- * ���\�b�h����̃R���g���[�����������Ȃ��C���[�W
+ * カプセル化について
+ * フィールド変数はprivateに
+ * メソッドでprivateの変数をコントロールするイメージ
  */
 public class TestBean {
-	
-	// �t�B�[���h�ϐ���private�錾
+
+	// フィールドの値はprivateに
 	private String name;
 	private int id;
-	
-	// �O������A�N�Z�X�����郁�\�b�h
+
+	// メソッドはpublicにしてフィールド変数を操作する
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int Id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
